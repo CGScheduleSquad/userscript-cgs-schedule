da8670cb2504730c942118a44a4bf1595850c3f9
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-09-09 20:42:05 -0700

    Update version number

--- userscripts/veracross-simple-schedule.js
+++ userscripts/veracross-simple-schedule.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Veracross CGS Schedule
-// @version      0.15
+// @version      0.16
 // @description  Bring the old CGS schedule view to the Veracross student schedule.
 // @author       Liam Wang & Tristan Peng
 // @namespace    https://greasyfork.org/users/118299
